bee763fe48e9ba153cf1e60b98f920cc0bb9a012
Pop4Phy <50183320+ton4phy@users.noreply.github.com>
2019-05-28 00:22:53 +0300

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
+
+$(document).ready(function(){
+    $("#nav-mobile").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
